a668d3d6c81b564dff354335586282a1989f018a
Natali Ustinova <nata@purple.studio>
2022-08-25 23:03:53 +0300

    feat: editProfile

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,9 +1,9 @@
-export default function PopupWithForm({name, isOpen, title, button, children, onClose}) {
+export default function PopupWithForm({name, isOpen, title, button, children, onClose, onSubmit}) {
   return (
     <div className={`popup ${name} ${isOpen && "popup_opened"}`}>
       <div className="popup__container">
         <h3 className="popup__heading">{title}</h3>
-        <form className={`popup__form ${name}`} name={name} noValidate>
+        <form className={`popup__form ${name}`} name={name} noValidate onSubmit={onSubmit}>
           {children}
           <button className="popup__save" type="submit">{button || 'Сохранить'}</button>
           <button onClick={onClose} className="popup__close" type="button" aria-label="Закрыть"></button>
