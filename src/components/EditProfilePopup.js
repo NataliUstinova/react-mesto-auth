a9b393ea763e42f0c21c313f4152bb9c428266b8
Natali Ustinova <nata@purple.studio>
2022-08-28 16:47:13 +0300

    feat: edit profile validation

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -8,13 +8,18 @@ export default function EditProfilePopup({isOpen, onClose, onUpdateUser, isLoadi
   const currentUser = useContext(CurrentUserContext);
   const [name, setName] = useState(currentUser.name);
   const [about, setAbout] = useState(currentUser.about);
-  
+  const [nameError, setNameError] = useState(false);
+  const [aboutError, setAboutError] = useState(false);
+
+  const buttonValidationClassName = (
+    `${nameError && 'popup__save_disabled' || aboutError && 'popup__save_disabled'}`
+  );
   
   useEffect(() => {
     setName(currentUser.name);
     setAbout(currentUser.about)
   }, [currentUser])
-
+  
   function handleNameChange(e) {
     setName(e.target.value);
   }
@@ -23,13 +28,22 @@ export default function EditProfilePopup({isOpen, onClose, onUpdateUser, isLoadi
     setAbout(e.target.value);
   }
   
+  useEffect( () => {
+    if (isOpen && name.length < 2) {setNameError(true);} else {setNameError(false);}
+  }, [name]);
+
+  useEffect( () => {
+    if (isOpen && about.length < 2) {setAboutError(true);} else {setAboutError(false);}
+  }, [about]);
+  
   function handleSubmit(e) {
     e.preventDefault();
-    onUpdateUser({name, about})
+    onUpdateUser({name, about});
   }
   
   return (
     <PopupWithForm
+      buttonValidationClassName={buttonValidationClassName}
       onClick={onClick}
       isOpen={isOpen}
       name="popup_profile-info"
@@ -41,24 +55,25 @@ export default function EditProfilePopup({isOpen, onClose, onUpdateUser, isLoadi
              required
              minLength="2"
              maxLength="40"
-             className="popup__input popup__input_value_name popup__text"
+             className={`popup__input popup__input_value_name popup__text ${nameError && 'popup__error_visible'}`}
              type="text"
              name="name"
              placeholder="Имя"
              defaultValue={currentUser.name}
              onChange={handleNameChange}/>
-      <span className="profile-name-error"></span>
+      <span className="popup__input_type_error">{nameError ? 'Имя должно быть больше 2х символов' : null}</span>
       <input id="profile-job"
              required
              minLength="2"
              maxLength="200"
-             className="popup__input popup__input_value_job popup__text"
+             className={`popup__input popup__input_value_job popup__text ${aboutError && 'popup__error_visible'}`}
              type="text"
              name="job"
              defaultValue={currentUser.about}
              placeholder="Деятельность"
              onChange={handleAboutChange}/>
       <span className="profile-job-error"></span>
+      <span className="popup__input_type_error">{aboutError ? 'должно быть больше 2х символов' : null}</span>
     </PopupWithForm>
   );
 };
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,11 +1,11 @@
-export default function PopupWithForm({name, isOpen, title, button, children, onClose, onSubmit, onClick}) {
+export default function PopupWithForm({name, isOpen, title, button, children, onClose, onSubmit, onClick, buttonValidationClassName}) {
   return (
     <div className={`${name} ${isOpen && "popup_opened"}`}>
       <div className="popup__container" onClick={onClick}>
         <h3 className="popup__heading">{title}</h3>
         <form className={`popup__form ${name}`} name={name} noValidate onSubmit={onSubmit}>
           {children}
-          <button className="popup__save" type="submit">{button || 'Сохранить'}</button>
+          <button className={`popup__save ${buttonValidationClassName}`} type="submit">{button || 'Сохранить'}</button>
           <button onClick={onClose} className="popup__close" type="button" aria-label="Закрыть"></button>
         </form>
       </div>
